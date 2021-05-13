13fbc52b8a3da65f3f9ae5352fe231cc70f21384
711-akanksha <68685034+711-akanksha@users.noreply.github.com>
2021-05-13 07:59:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import speech_recognition as sr
 
 try:
     server = smtplib.SMTP('smtp.gmail.com' , 587)
-    server.starttls()
-    server.login("akanksha1116gupta@gmail.com","xyz")
+    server.starttls()                                                                                        
+    server.login("uniqueQueue87898@gmail.com","pas")
+
 
     pyttsx3.speak("To whom you want to send email")
     listener = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         listener.adjust_for_ambient_noise(source)
-        audio = listener.listen(source, None, 10)
+        audio = listener.listen(source)
         command = listener.recognize_google(audio)
         EmailTo = str(command).lower().replace(" at ", "@").replace(" ", "").replace("at", "@")
         print(EmailTo)
@@ -23,15 +24,15 @@ try:
     with sr.Microphone() as source:
         print("Listening...")
         listener.adjust_for_ambient_noise(source)
-        audio = listener.listen(source)
+        audio = listener.listen(source, None, 10)
         command = listener.recognize_google(audio)
         Content = str(command)
-        print(" hel me out")
+        print(Content)
 
-    server.sendmail("akanksha1116gupta@gmail.com", "bakuaku738@gmail.com", "made new project")
+    server.sendmail("uniqueQueue87898@gmail.com", "EmailTo", "Content")
     pyttsx3.speak("Email sent Successfully!")
-    print("Email sent Successfully!" + akanksha1116gupta@gmail.com ,+bakuaku738@gmail.com)
+    print("Email sent Successfully!" + To ,+ EmailTo )
 
 except Exception as e:
     print(e)
-    pyttsx3.speak("SomeThing Went Wrong, Please Try Again!")
+    pyttsx3.speak("SomeThing Went Wrong, Please Try Again!"   )
